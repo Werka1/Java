7d68df79bf4e97f6bbd6418282fbd7d166668254
Weronika <wgruzlewska@gmail.com>
2022-03-16 12:08:35 +0100

    simple code

--- src/InstrukcjeWarunkoweZad.java
+++ src/InstrukcjeWarunkoweZad.java
@@ -4,12 +4,15 @@ public class InstrukcjeWarunkoweZad {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swój wiek");
         int age = scanner.nextInt();
 
-        if (age >= 18){
+        if (age >= 18 && age >= 0){
             System.out.println("Dziękujemy za zakupy");
+        }else if (age <0 ) {
+            System.out.println("Wprowadź poprawną wartość");
         }else {
-            System.out.println("Jesteś niepełnoletni, sprzedaż alkoholu zabroniona");
+            System.out.println("Jesteś niepełnoletni, nie możesz kupić alkoholu");
         }
     }
 }
