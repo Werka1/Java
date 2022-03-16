b3778886b1f398ede41547473123b3cf10318b4b
Weronika <wgruzlewska@gmail.com>
2022-03-16 12:49:06 +0100

    simple loop "For"

---
+++ src/ForLoop.java
@@ -0,0 +1,24 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+
+        // i jest spacjalną zmienną sterującą
+        for (int i = 0; i < 100; i =i+2) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 5 == 0)
+                System.out.println(i);
+        }
+         int number = 12;
+
+        for (int i =0 ; i<number ; i++){
+            System.out.println("Bedę robić pracę domową");
+        }
+
+
+
+
+    }
+}
