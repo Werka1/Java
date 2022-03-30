a3c380b5aabdfbdf9f2788a1f0eff999733eea59
Weronika <wgruzlewska@gmail.com>
2022-03-30 09:57:11 +0200

    sample commit message

---
+++ src/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+
+    public String imie;
+    public String nazwisko;
+    public String nick;
+    public String email;
+    public int numerIndexu;
+
+    public void przedstawSie(){
+        System.out.println("Nazywam się " + imie + " " + nazwisko);
+    }
+
+    public void zalogujSie(){
+        System.out.println("Loguje się za pomocą " + nick);
+    }
+
+    public void podajNrIndeksu(){
+        System.out.println("Mój numer indeksu to: " + numerIndexu);
+    }
+
+    public void podajEmail(){
+        System.out.println("Mój email to: " + email
+        );
+    }
+
+
+}
